3ff97f5424e82796bebf5b7e45bfad145d85f184
Omar Brikaa <brikaaomar@gmail.com>
2023-11-23 16:29:28 +0200

    WIP pseudocode

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+struct Solution {
+    chromosome: Vec<f64>,
+    fitness: f64,
+}
+
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+fn create_initialize_population(lb: f64, ub: f64, degree: i32, pop_size: i32) {}
+
+fn main() {
+    println!("Hello, world!");
+}
